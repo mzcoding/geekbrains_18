039ab9cc652f8c499991035acad92d9b4344456a
Stanislav <stanislav.boyko@orderyoyo.com>
2023-01-14 22:54:57 +0400

    Init commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\NewsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => ''], static function() {
+   Route::get('/news', [NewsController::class, 'index'])
+    ->name('news');
+   Route::get('/news/{id}/show', [NewsController::class, 'show'])
+   ->where('id', '\d+')
+       ->name('news.show');
+});
+
+
